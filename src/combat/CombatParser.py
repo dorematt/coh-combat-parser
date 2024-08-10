6c8c19492eedb26bd0dd9e962089e15e790d10a6
Tenkay <26863406+dorematt@users.noreply.github.com>
2024-08-10 19:02:29 +1000

    fixed broken pet ability activation counts

--- src/combat/CombatParser.py
+++ src/combat/CombatParser.py
@@ -288,7 +288,7 @@ class Parser(QObject):
 
             # Ability activation for the pet
             char_ability = char.abilities[this_ability]
-            if char.get_type == "pet": char_ability.ability_used() # We don't have power activation events for pets, so we'll use the hit roll as a proxy for ability usage
+            if char.get_type() == "pet": char_ability.ability_used() # We don't have power activation events for pets, so we'll use the hit roll as a proxy for ability usage
             char_ability.ability_hit(data["outcome"] == "HIT") 
 
         
