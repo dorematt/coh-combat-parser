c2765ec106e38fd8f94e87335ccdd790ea4e174d
Tenkay <26863406+dorematt@users.noreply.github.com>
2024-08-25 20:28:03 +1000

    Rearranged settings UI to be a bit cleaner

--- src/ui/Settings.py
+++ src/ui/Settings.py
@@ -11,17 +11,23 @@ class SettingsWindow(QDialog):
         super().__init__()
 
         self.setWindowTitle("Settings")
-        #self.setGeometry(100, 100, 300, 200)
+
 
         self.settings = QSettings(Globals.AUTHOR, Globals.APPLICATION_NAME)
 
         self.layout = QVBoxLayout()
 
-        # Combat Session Timeout
+        # ---   Combat Sessions   ---
+        combat_session_group = QGroupBox("Combat Sessions")
+        self.sesison_name_layout = QVBoxLayout(combat_session_group)
+        # self.layout.addLayout(self.sesison_name_layout)
+
+        # ---Combat Session Timeout---
+        combat_session_timeout_row = QHBoxLayout()
         tooltip = "The amount of time in seconds to wait after the last action before ending a combat session"
-        timeout_label = QLabel("Combat Session Timeout:")
+        timeout_label = QLabel("Auto Timeout: ")
         timeout_label.setToolTip(tooltip)
-        self.layout.addWidget(timeout_label)
+        combat_session_timeout_row.addWidget(timeout_label)
 
         timeout_spinbox = QSpinBox()
         timeout_spinbox.setToolTip(tooltip)
@@ -29,58 +35,22 @@ class SettingsWindow(QDialog):
         timeout_spinbox.setMaximum(9999)
         timeout_spinbox.setValue(self.settings.value("CombatSessionTimeout", Globals.DEFAULT_COMBAT_SESSION_TIMEOUT, int))
         timeout_spinbox.valueChanged.connect(self.save_combat_session_timeout)
-        self.layout.addWidget(timeout_spinbox)
+        combat_session_timeout_row.addWidget(timeout_spinbox)
+        self.sesison_name_layout.addLayout(combat_session_timeout_row)
 
+        timeout_tail_label = QLabel("seconds")
+        combat_session_timeout_row.addWidget(timeout_tail_label)
 
+        divider = QLabel("")
+        divider.setFixedHeight(10)
+        self.sesison_name_layout.addWidget(divider)
 
-        #Associate Procs to Powers
-        tooltip = "Toggle whether to associate procs to powers or make them their own ability"
-        proc_label = QLabel("Associate Procs to Powers:")
-        proc_label.setToolTip(tooltip)
+        # ---Default Session Naming---
 
-        proc_checkbox = QCheckBox(checked=self.settings.value("AssociateProcsToPowers", Globals.DEFAULT_ASSOCIATE_PROCS_TO_POWERS, bool))
-        proc_checkbox.setToolTip(tooltip)
-        proc_checkbox.stateChanged.connect(self.save_associate_procs_to_powers)
-
-        proc_layout = QHBoxLayout()
-        proc_layout.addWidget(proc_label)
-        proc_layout.addWidget(proc_checkbox)
-        self.layout.addLayout(proc_layout)
-
-        # Automatically update to latest file
-        tooltip = "Automatically check and set the filepath to the latest log file"
-        auto_update_label = QLabel("Get Latest Log File:")
-        auto_update_label.setToolTip(tooltip)
-
-        auto_update_checkbox = QCheckBox()
-        auto_update_checkbox.setToolTip(tooltip)
-        auto_update_checkbox.setChecked(self.settings.value("AutoUpdateLogFile", Globals.DEFAULT_AUTO_UPDATE_LOG_FILE, bool))
-  
-        auto_update_layout = QHBoxLayout()
-        auto_update_layout.addWidget(auto_update_label)
-        auto_update_layout.addWidget(auto_update_checkbox)
-        self.layout.addLayout(auto_update_layout)
-        
-        # Console Verbosity
-        tooltip = "The amount of information to display in the console - For Debug purposes only, this WILL impact performance"
-        verbosity_label = QLabel("Console Verbosity:")
-        verbosity_label.setToolTip(tooltip)
-        self.layout.addWidget(verbosity_label)
-
-        verbosity_spinbox = QSpinBox()
-        verbosity_spinbox.setToolTip(tooltip)
-        verbosity_spinbox.setMinimum(0)
-        verbosity_spinbox.setMaximum(4)
-        verbosity_spinbox.setValue(self.settings.value("ConsoleVerbosity", Globals.DEFAULT_CONSOLE_VERBOSITY, int))
-        verbosity_spinbox.valueChanged.connect(self.save_console_verbosity)
-        self.layout.addWidget(verbosity_spinbox)
-
-        session_naming_group = QGroupBox("Default Session Naming")
-        self.sesison_name_layout = QVBoxLayout(session_naming_group)
-        self.layout.addLayout(self.sesison_name_layout)
-
-        # Default Session Naming
         tooltip = "Sets the default naming behaviour for combat sessions"
+        naming_label = QLabel("Default Session Naming Style:")
+        naming_label.setToolTip(tooltip)
+        self.sesison_name_layout.addWidget(naming_label)
 
         self.naming_combobox = QComboBox()
         self.naming_combobox.setToolTip(tooltip)
@@ -93,7 +63,7 @@ class SettingsWindow(QDialog):
         self.naming_combobox.currentIndexChanged.connect(self.update_user_default_session_name)
         self.sesison_name_layout.addWidget(self.naming_combobox)
 
-        # User Specified Name Prefix
+        # ---User Specified Name Prefix---
         tooltip = "The prefix to use when naming a combat session with the User Specified naming behaviour"
         self.user_prefix_label = QLabel("User Specified Name:")
         self.user_prefix_label.setToolTip(tooltip)   
@@ -109,7 +79,67 @@ class SettingsWindow(QDialog):
             self.user_prefix_label.setVisible(False)
         self.sesison_name_layout.addWidget(self.user_prefix_lineedit)
 
-        self.layout.addWidget(session_naming_group)
+        self.layout.addWidget(combat_session_group)
+
+        # ---   Powers Settings   ---
+        powers_group = QGroupBox("Powers")
+        powers_group_layout = QVBoxLayout(powers_group)
+
+        # ---Associate Procs to Powers---
+        tooltip = "Toggle whether to associate procs to powers or make them their own ability"
+        proc_label = QLabel("Associate Procs to Powers:")
+        proc_label.setToolTip(tooltip)
+
+        proc_checkbox = QCheckBox(checked=self.settings.value("AssociateProcsToPowers", Globals.DEFAULT_ASSOCIATE_PROCS_TO_POWERS, bool))
+        proc_checkbox.setToolTip(tooltip)
+        proc_checkbox.stateChanged.connect(self.save_associate_procs_to_powers)
+
+        proc_row = QHBoxLayout()
+        proc_row.addWidget(proc_label)
+        proc_row.addStretch()
+        proc_row.addWidget(proc_checkbox)
+        powers_group_layout.addLayout(proc_row)
+
+        self.layout.addWidget(powers_group)
+
+        # ---   Misc Settings   ---
+        misc_group = QGroupBox("Misc")
+        misc_group_layout = QVBoxLayout(misc_group)
+
+
+        # ---Automatically update to latest file---
+        tooltip = "Automatically check and set the filepath to the latest log file"
+        auto_update_label = QLabel("Get Latest Log File:")
+        auto_update_label.setToolTip(tooltip)
+
+        auto_update_checkbox = QCheckBox()
+        auto_update_checkbox.setToolTip(tooltip)
+        auto_update_checkbox.setChecked(self.settings.value("AutoUpdateLogFile", Globals.DEFAULT_AUTO_UPDATE_LOG_FILE, bool))
+  
+        auto_update_row = QHBoxLayout()
+        auto_update_row.addWidget(auto_update_label)
+        auto_update_row.addStretch()
+        auto_update_row.addWidget(auto_update_checkbox)
+        misc_group_layout.addLayout(auto_update_row)
+        
+        # ---Console Verbosity---
+        tooltip = "The amount of information to display in the console - For Debug purposes only, this WILL impact performance"
+        verbosity_label = QLabel("Console Verbosity:")
+        verbosity_label.setToolTip(tooltip)
+
+        verbosity_spinbox = QSpinBox()
+        verbosity_spinbox.setToolTip(tooltip)
+        verbosity_spinbox.setMinimum(0)
+        verbosity_spinbox.setMaximum(4)
+        verbosity_spinbox.setValue(self.settings.value("ConsoleVerbosity", Globals.DEFAULT_CONSOLE_VERBOSITY, int))
+        verbosity_spinbox.valueChanged.connect(self.save_console_verbosity)
+
+        verbosity_row = QHBoxLayout()
+        verbosity_row.addWidget(verbosity_label)
+        verbosity_row.addWidget(verbosity_spinbox)
+        misc_group_layout.addLayout(verbosity_row)
+
+        self.layout.addWidget(misc_group)
 
 
         # Save button
